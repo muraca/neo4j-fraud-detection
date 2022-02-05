9725e89179a7393daff688d21fb5b5bb394f3aaa
muraca <mmuraca247@gmail.com>
2022-02-05 09:51:58 +0100

    merge on create

--- scripts.py
+++ scripts.py
@@ -8,23 +8,27 @@ def drop_content(tx):
 
 def create_customer(tx, PATH):
     tx.run ("LOAD CSV WITH HEADERS FROM \"" + PATH + "\" AS row " + 
-            "CREATE (:Customer {customer_id : row.customer_id, x_customer_id : row.x_customer_id, " + 
-            "y_customer_id : row.y_customer_id, mean_amount : row.mean_amount, " +
-            "std_amount : row.std_amount, mean_nb_tx_per_day : row.mean_nb_tx_per_day})")
+            "MERGE (c:Customer {customer_id : row.customer_id}) " +
+            "SET c = { customer_id : row.customer_id, " +
+            "x_customer_id : row.x_customer_id, y_customer_id : row.y_customer_id, " +
+            "mean_amount : row.mean_amount, std_amount : row.std_amount, " + 
+            "mean_nb_tx_per_day : row.mean_nb_tx_per_day }")
 
 def create_terminal(tx, PATH):
     tx.run ("LOAD CSV WITH HEADERS FROM \"" + PATH + "\" AS row " + 
-            "CREATE (:Terminal {terminal_id : row.terminal_id, x_terminal_id : row.x_terminal_id, " + 
-            "y_terminal_id : row.y_terminal_id})")
+            "MERGE (t:Terminal { terminal_id : row.terminal_id }) " + 
+            "SET t = { terminal_id = row.terminal_id," +
+            "x_terminal_id : row.x_terminal_id, y_terminal_id : row.y_terminal_id }")
 
 def create_transaction(tx, PATH):
-    tx.run ("LOAD CSV WITH HEADERS FROM \"" + PATH + "\" AS row " + 
+    tx.run ("LOAD CSV WITH HEADERS FROM \"" + PATH + "\"  AS row " +  
             "MERGE (c:Customer {customer_id : row.customer_id}) " +
             "MERGE (t:Terminal {terminal_id : row.terminal_id}) " +
-            "MERGE (c)-[tx:TRANSACTION{transaction_id : row.transaction_id}]->(t) " +
-            "SET tx += {tx_datetime : datetime({epochSeconds: toInteger(row.tx_datetime)}), tx_amount : row.tx_amount, " +
+            "MERGE (c)-[tx:TRANSACTION{ transaction_id : row.transaction_id }]->(t) " +
+            "SET tx = { transaction_id = row.transaction_id, tx_amount : row.tx_amount, " +    
+            "tx_datetime : datetime({epochSeconds: toInteger(row.tx_datetime)}), " +
             "tx_fraud : row.tx_fraud, period_of_the_day : row.period_of_the_day, " +
-            "kind_of_products : tx.kind_of_products}")
+            "kind_of_products : row.kind_of_products } ")
             
 s = "1"
 # path dalla root, altrimenti Neo4J non riesce a leggere
